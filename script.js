4ce59b11046e3a607abde9b6683145df38080543
Aleksandra-Stempin <36968259+Aleksandra-Stempin@users.noreply.github.com>
2024-04-24 14:10:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -113,6 +113,9 @@ function guessLetter() {
 		document.getElementById("usedLetters").textContent=""
 		
 		document.getElementById("test").textContent=""
+		document.getElementById('chancesCounterLabel_1').textContent=";
+		document.getElementById('chancesCounter').textContent=";
+		document.getElementById('chancesCounterLabel_2').textContent="";
 		
 		document.getElementById("guessLetter").disabled=true;
 		document.getElementById("guessWord").disabled=true;
